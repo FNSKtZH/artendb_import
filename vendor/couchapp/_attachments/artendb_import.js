00eea68bdc98b9bb4f252879f5183789195d0ca7
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-06-28 15:47:25 +0200

    Floraimport inkl. Datensammlungen realisiert. Uff

--- vendor/couchapp/_attachments/artendb_import.js
+++ vendor/couchapp/_attachments/artendb_import.js
@@ -44,40 +44,20 @@ function importiereFauna_02(myDB, startwert, endwert) {
 	}
 }
 
-function importiereFlora() {
-	var myDB = new ACCESSdb("C:\\Users\\alex\\artendb_import\\export_in_json.mdb", {showErrors:true});
-	$.ajax({
-		type: "POST", 
-		url: "http://127.0.0.1:5984/_session",
-		dataType: "json",
-				data: {name: 'barbalex', password: 'dLhdMg12'},
-				beforeSend: function(xhr) {
-						xhr.setRequestHeader('Accept', 'application/json');
-				},
-				success: function (data) {
-					//DB übergeben und Anfangswert 1
-					importiereFlora_02(myDB);
-				}
-	});
-}
-
-function importiereFlora_02(myDB) {
+function importiereFlora(myDB) {
 	var Datensammlungen, sqlDatensammlungen, Index, Datensamlung, DsDerDatensammlung, Art, DsObjekt, Guid;
-	sqlDatensammlungen = "SELECT * FROM tblDatensammlung WHERE DsIndex = 'tblFloraSisf' AND DsBeziehungstyp = '1_zu_1' ORDER BY DsReihenfolge";
+	sqlDatensammlungen = "SELECT * FROM tblDatensammlungMetadaten WHERE DsIndex = 'tblFloraSisf' AND DsBeziehungstyp = '1_zu_1' ORDER BY DsReihenfolge";
 	Datensammlungen = frageSql(myDB, sqlDatensammlungen);
 	//Index importieren
 	for (i in Datensammlungen) {
 		//alert("Datensammlungen[i].DsTabelle = " + Datensammlungen[i].DsTabelle);
 		if (Datensammlungen[i].DsTabelle === Datensammlungen[i].DsIndex) {
 			Index = frageSql(myDB, "SELECT * FROM " + Datensammlungen[i].DsTabelle);
-			//alert("Index = " + JSON.stringify(Index));
 			for (x in Index) {
 				//Art als Objekt gründen
 				Art = {};
 				//_id soll GUID sein, aber ohne Klammern
-				Art._id = Index[x].GUID.slice(1, 36);
-				//alert("Art = " + JSON.stringify(Art));
-				//alert("Datensammlungen[i].DsName = " + Datensammlungen[i].DsName);
+				Art._id = Index[x].GUID.slice(1, 37);
 				//Datensammlung als Objekt gründen, heisst wie DsName
 				Art[Datensammlungen[i].DsName] = {};
 				Art[Datensammlungen[i].DsName].Typ = "Datensammlung";
@@ -89,55 +69,86 @@ function importiereFlora_02(myDB) {
 						if (y !== "GUID") {
 							Art[Datensammlungen[i].DsName].Felder[y] = Index[x][y];
 						} else {
-							Art[Datensammlungen[i].DsName].Felder[y] = Index[x][y].slice(1, 36);
+							Art[Datensammlungen[i].DsName].Felder[y] = Index[x][y].slice(1, 37);
 						}
 					}
 				}
-				//alert("Art = " + JSON.stringify(Art));
 				$db = $.couch.db("artendb");
 				$db.saveDoc(Art);
 			}
-			//importiereJsonObjekt(Art);
 		}
 		break;
 	}
-	//danach durch alle übrigen Datensammlungen loopen und ihre Felder in ein Objekt mit Namen und Eigenschaften der Datensammlung einfügen
-	for (i in Datensammlungen) {
-		//Nur Datensammlungen, die nicht der Index sind
-		if (Datensammlungen[i].DsTabelle !== Datensammlungen[i].DsIndex) {
-			//Datensätze der Datensammlung abfragen
-			DsDerDatensammlung = frageSql(myDB, "SELECT * FROM " + Datensammlungen[i].DsTabelle);
-			//alert("DsDerDatensammlung = " + JSON.stringify(DsDerDatensammlung));
-			for (x in DsDerDatensammlung) {
-				//Datensammlung als Objekt gründen
-				Datensammlung = {};
-				Datensammlung.Typ = "Datensammlung";
-				//Felder der Datensammlung als Objekt gründen
-				Datensammlung.Felder = {};
-				//Felder anfügen, wenn sie Werte enthalten
-				for (y in DsDerDatensammlung[x]) {
-					if (y !== "id" && DsDerDatensammlung[x][y] !== "" && DsDerDatensammlung[x][y] !== null) {
-						Datensammlung.Felder[y] = DsDerDatensammlung[x][y];
-					}
-				}
-				//entsprechenden Index öffnen
-				$db = $.couch.db("artendb");
-				//GUID herausfinden
-				//alert("Datensammlungen[i].DsBeziehungsfeldDs = " + Datensammlungen[i].DsBeziehungsfeldDs);
-				Guid = frageSql(myDB, "SELECT GUID FROM tblFloraSisf WHERE NR = " + DsDerDatensammlung[x][Datensammlungen[i].DsBeziehungsfeldDs]);
-				//alert("Guid[0].GUID.slice(1, 36) = " + Guid[0].GUID.slice(1, 36));
-				$db.openDoc(Guid[0].GUID.slice(1, 36), {
-					success: function (doc) {
-						//Datensammlung anfügen
-						doc[Datensammlungen[i].DsName] = Datensammlung;
-						alert("doc[Datensammlungen[i].DsName] = " + JSON.stringify(doc[Datensammlungen[i].DsName]));
-						//in DB speichern
-						$db.saveDoc(doc);
-					}
-				});
+}
+
+function importiereFloraDatensammlungen(tblName) {
+	initiiereImport(importiereFloraDatensammlungen_02, tblName);
+}
+
+function importiereFloraDatensammlungen_02(myDB, tblName) {
+	var DatensammlungMetadaten, Datensammlung, sqlDatensammlung, DatensammlungDieserArt, anzFelder;
+	DatensammlungMetadaten = frageSql(myDB, "SELECT * FROM tblDatensammlungMetadaten WHERE DsTabelle = '" + tblName + "'");
+	//Datensätze der Datensammlung abfragen, mit GUID ergänzen
+	sqlDatensammlung = "SELECT * FROM " + tblName + " INNER JOIN tblFloraSisfGuid ON tblFloraSisfGuid.NR = " + tblName + "." + DatensammlungMetadaten[0].DsBeziehungsfeldDs;
+	Datensammlung = frageSql(myDB, sqlDatensammlung);
+	for (x in Datensammlung) {
+		//Datensammlung als Objekt gründen
+		DatensammlungDieserArt = {};
+		DatensammlungDieserArt.Typ = "Datensammlung";
+		//Felder der Datensammlung als Objekt gründen
+		DatensammlungDieserArt.Felder = {};
+		//Felder anfügen, wenn sie Werte enthalten
+		anzFelder = 0;
+		for (y in Datensammlung[x]) {
+			if (y !== "id" && y !== "GUID" && y !== "NR" && Datensammlung[x][y] !== "" && Datensammlung[x][y] !== null && y !== DatensammlungMetadaten[0].DsBeziehungsfeldDs && y !== "Gruppe") {
+				DatensammlungDieserArt.Felder[y] = Datensammlung[x][y];
+				anzFelder += 1;
 			}
 		}
+		//entsprechenden Index öffnen
+		//sicherstellen, dass Felder vorkommen. Gibt sonst einen Fehler
+		if (anzFelder > 0) {
+			//Datenbankabfrage ist langsam. Estern aufrufen, 
+			//sonst überholt die for-Schlaufe und DatensammlungDieserArt ist bis zur saveDoc-Ausführung eine andere!
+			fuegeDatensammlungZuArt(Datensammlung[x].GUID, DatensammlungMetadaten[0].DsName, DatensammlungDieserArt);
+		}
 	}
+}
+
+function fuegeDatensammlungZuArt(GUID, DsName, DatensammlungDieserArt) {
+	$db = $.couch.db("artendb");
+	$db.openDoc(GUID, {
+		success: function (doc) {
+			//Datensammlung anfügen
+			doc[DsName] = DatensammlungDieserArt;
+			//in artendb speichern
+			$db.saveDoc(doc);
+		}
+	});
+}
+
+function initiiereImport(functionName, tblName) {
+	var myDB = new ACCESSdb("C:\\Users\\alex\\artendb_import\\export_in_json.mdb", {showErrors:true});
+	$.ajax({
+		type: "POST", 
+		url: "http://127.0.0.1:5984/_session",
+		dataType: "json",
+		data: {
+			name: 'barbalex', 
+			password: 'dLhdMg12'
+		},
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('Accept', 'application/json');
+		},
+		success: function (data) {
+			//DB übergeben
+			if (tblName) {
+				eval(functionName(myDB, tblName));
+			} else {
+				eval(functionName(myDB));
+			}
+		}
+	});
 }
 
 //nimmt die DB und einen sql-String entgegen
