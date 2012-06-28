06b8be9f3a230d320e84b9dd7972730281848de4
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-06-29 00:21:27 +0200

    Fauna, Datensammlungen importieren: Automatische Aufteilung auf Batches von 5'000 Datensätzen

--- vendor/couchapp/_attachments/artendb_import.js
+++ vendor/couchapp/_attachments/artendb_import.js
@@ -1,49 +1,3 @@
-function importiereFauna() {
-	var myDB = new ACCESSdb("C:\\Users\\alex\\artendb_import\\export_in_json.mdb", {showErrors:true});
-	$.ajax({
-		type: "POST", 
-		url: "http://127.0.0.1:5984/_session",
-		dataType: "json",
-				data: {name: 'barbalex', password: 'dLhdMg12'},
-				beforeSend: function(xhr) {
-						xhr.setRequestHeader('Accept', 'application/json');
-				},
-				success: function (data) {
-					//DB übergeben und Anfangswert 1
-					importiereFauna_02(myDB, 1);
-				}
-	});
-}
-
-function importiereFauna_02(myDB, startwert, endwert) {
-	//die ersten 10 posts reagieren nicht!!!!!!??????
-	//darum am Ende nochmals die ersten 10 aufrufen
-	if (!endwert) {
-		endwert = startwert + 50;
-	}
-	d = frageSql(myDB, "SELECT * FROM tblFaunaExport WHERE id >= " + startwert + " AND id <= " + endwert);
-	for (i in d) {
-		//_id soll GUID sein
-		d[i]._id = d[i].fns_Guid;
-		//leerwerte entfernen
-		for (y in d[i]) {
-			if (y === "id" || d[i][y] === "" || d[i][y] === null) {
-				delete d[i][y];
-			}
-		}
-	}
-	importiereJsonObjekt(d);
-	//weiter mit den nächsten x Datensätzen, wenn tblFaunaExport weitere Datensätze enthält
-	//alert(d.length);
-	if (d.length > 50) {
-		importiereFauna_02(myDB, endwert);
-	} else {
-		//die ersten 10 posts reagieren nicht!!!!!!??????
-		//darum am Ende nochmals die ersten 10 aufrufen
-		importiereFauna_02(myDB, 1, 500);
-	}
-}
-
 function importiereFlora(myDB) {
 	var Datensammlungen, sqlDatensammlungen, Index, Datensamlung, DsDerDatensammlung, Art, DsObjekt, Guid;
 	sqlDatensammlungen = "SELECT * FROM tblDatensammlungMetadaten WHERE DsIndex = 'tblFloraSisf' AND DsBeziehungstyp = '1_zu_1' ORDER BY DsReihenfolge";
@@ -82,7 +36,7 @@ function importiereFlora(myDB) {
 }
 
 function importiereFloraDatensammlungen(tblName) {
-	initiiereImport(importiereFloraDatensammlungen_02, tblName);
+	initiiereImport("importiereFloraDatensammlungen_02", tblName);
 }
 
 function importiereFloraDatensammlungen_02(myDB, tblName) {
@@ -115,6 +69,82 @@ function importiereFloraDatensammlungen_02(myDB, tblName) {
 	}
 }
 
+function importiereFauna(myDB) {
+	var Datensammlungen, sqlDatensammlungen, Index, Datensamlung, DsDerDatensammlung, Art, DsObjekt, Guid, dsNr;
+	sqlDatensammlungen = "SELECT * FROM tblDatensammlungMetadaten WHERE DsTabelle = 'tblFaunaCscf'";
+	Datensammlungen = frageSql(myDB, sqlDatensammlungen);
+	//Index importieren
+	Index = frageSql(myDB, "SELECT * FROM tblFaunaCscf");
+	dsNr = 0;
+	for (x in Index) {
+		//In Häppchen von max. 8000 Datensätzen aufteilen
+		dsNr += 1;
+		if ((sessionStorage.fauna === "fauna1" && dsNr < 8000) || (sessionStorage.fauna === "fauna2" && dsNr >= 8000 && dsNr < 16000) || (sessionStorage.fauna === "fauna3" && dsNr >= 16000)) {
+			//Art als Objekt gründen
+			Art = {};
+			//_id soll GUID sein, aber ohne Klammern
+			Art._id = Index[x].GUID.slice(1, 37);
+			//Datensammlung als Objekt gründen, heisst wie DsName
+			Art[Datensammlungen[0].DsName] = {};
+			Art[Datensammlungen[0].DsName].Typ = "Datensammlung";
+			//Felder der Datensammlung als Objekt gründen
+			Art[Datensammlungen[0].DsName].Felder = {};
+			//Felder anfügen, wenn sie Werte enthalten
+			for (y in Index[x]) {
+				if (y !== "id" && Index[x][y] !== "" && Index[x][y] !== null) {
+					if (y !== "GUID") {
+						Art[Datensammlungen[0].DsName].Felder[y] = Index[x][y];
+					} else {
+						Art[Datensammlungen[0].DsName].Felder[y] = Index[x][y].slice(1, 37);
+					}
+				}
+			}
+			$db = $.couch.db("artendb");
+			$db.saveDoc(Art);
+		}
+	}
+	delete sessionStorage.fauna;
+}
+
+function importiereFaunaDatensammlungen(tblName, Anz) {
+	initiiereImport("importiereFaunaDatensammlungen_02", tblName, Anz);
+}
+
+function importiereFaunaDatensammlungen_02(myDB, tblName, Anz) {
+	var DatensammlungMetadaten, Datensammlung, sqlDatensammlung, DatensammlungDieserArt, anzFelder, anzDs;
+	DatensammlungMetadaten = frageSql(myDB, "SELECT * FROM tblDatensammlungMetadaten WHERE DsTabelle = '" + tblName + "'");
+	//Datensätze der Datensammlung abfragen, mit GUID ergänzen
+	sqlDatensammlung = "SELECT * FROM " + tblName + " INNER JOIN tblFaunaCscfGuid ON tblFaunaCscfGuid.NR = " + tblName + "." + DatensammlungMetadaten[0].DsBeziehungsfeldDs;
+	Datensammlung = frageSql(myDB, sqlDatensammlung);
+	anzDs = 0;
+	for (x in Datensammlung) {
+		anzDs += 1;
+		//nur importieren, wenn innerhalb des mit Anz übergebenen 8000er Batches
+		if ((anzDs > (Anz*5000-5000)) && (anzDs <= Anz*5000)) {
+			//Datensammlung als Objekt gründen
+			DatensammlungDieserArt = {};
+			DatensammlungDieserArt.Typ = "Datensammlung";
+			//Felder der Datensammlung als Objekt gründen
+			DatensammlungDieserArt.Felder = {};
+			//Felder anfügen, wenn sie Werte enthalten
+			anzFelder = 0;
+			for (y in Datensammlung[x]) {
+				if (y !== "id" && y !== "GUID" && y !== "Nuesp" && Datensammlung[x][y] !== "" && Datensammlung[x][y] !== null && y !== DatensammlungMetadaten[0].DsBeziehungsfeldDs && y !== "Gruppe") {
+					DatensammlungDieserArt.Felder[y] = Datensammlung[x][y];
+					anzFelder += 1;
+				}
+			}
+			//entsprechenden Index öffnen
+			//sicherstellen, dass Felder vorkommen. Gibt sonst einen Fehler
+			if (anzFelder > 0) {
+				//Datenbankabfrage ist langsam. Estern aufrufen, 
+				//sonst überholt die for-Schlaufe und DatensammlungDieserArt ist bis zur saveDoc-Ausführung eine andere!
+				fuegeDatensammlungZuArt(Datensammlung[x].GUID, DatensammlungMetadaten[0].DsName, DatensammlungDieserArt);
+			}
+		}
+	}
+}
+
 function fuegeDatensammlungZuArt(GUID, DsName, DatensammlungDieserArt) {
 	$db = $.couch.db("artendb");
 	$db.openDoc(GUID, {
@@ -127,9 +157,11 @@ function fuegeDatensammlungZuArt(GUID, DsName, DatensammlungDieserArt) {
 	});
 }
 
-function initiiereImport(functionName, tblName) {
+function initiiereImport(functionName, tblName, Anz) {
 	var myDB;
+	//mit der mdb verbinden
 	myDB = verbindeMitMdb();
+	//in der Couch anmelden
 	$.ajax({
 		type: "POST",
 		url: "http://127.0.0.1:5984/_session",
@@ -144,9 +176,9 @@ function initiiereImport(functionName, tblName) {
 		success: function (data) {
 			//DB übergeben
 			if (tblName) {
-				eval(functionName(myDB, tblName));
+				eval(functionName + "(myDB, tblName, Anz)");
 			} else {
-				eval(functionName(myDB));
+				eval(functionName + "(myDB)");
 			}
 		}
 	});
@@ -191,21 +223,40 @@ function importiereJsonObjekt(JsonObjekt) {
 	});
 }
 
-function baueFloraDatensammlungenSchaltflächenAuf() {
-	var Datensammlungen, sqlDatensammlungen, myDB, html;
+function baueDatensammlungenSchaltflächenAuf() {
+	var DatensammlungenFlora, sqlDatensammlungenFlora, DatensammlungenFauna, sqlDatensammlungenFauna, myDB, html, qryAnzDs, anzDs, anzButtons;
 	myDB = verbindeMitMdb();
-	sqlDatensammlungen = "SELECT * FROM tblDatensammlungMetadaten WHERE DsIndex = 'tblFloraSisf' AND DsBeziehungstyp = '1_zu_1' AND DsTabelle <> 'tblFloraSisf' ORDER BY DsReihenfolge";
-	Datensammlungen = frageSql(myDB, sqlDatensammlungen);
-	if (Datensammlungen) {
+	sqlDatensammlungenFlora = "SELECT * FROM tblDatensammlungMetadaten WHERE DsIndex = 'tblFloraSisf' AND DsBeziehungstyp = '1_zu_1' AND DsTabelle <> 'tblFloraSisf' ORDER BY DsReihenfolge";
+	DatensammlungenFlora = frageSql(myDB, sqlDatensammlungenFlora);
+	if (DatensammlungenFlora) {
 		html = "";
-		for (i in Datensammlungen) {
+		for (i in DatensammlungenFlora) {
 			html += "<button id='";
-			html += Datensammlungen[i].DsTabelle;
+			html += DatensammlungenFlora[i].DsTabelle;
 			html += "' name='SchaltflächeFloraDatensammlung'>";
-			html += Datensammlungen[i].DsName;
+			html += DatensammlungenFlora[i].DsName;
 			html += "</button>";
 		}
 		$("#SchaltflächenFloraDatensammlungen").html(html);
+		//jetzt Fauna
+		sqlDatensammlungenFauna = "SELECT * FROM tblDatensammlungMetadaten WHERE DsIndex = 'tblFaunaCscf' AND DsBeziehungstyp = '1_zu_1' AND DsTabelle <> 'tblFaunaCscf' ORDER BY DsReihenfolge";
+		DatensammlungenFauna = frageSql(myDB, sqlDatensammlungenFauna);
+		html = "";
+		for (i in DatensammlungenFauna) {
+			//Anzahl Datensätze ermitteln
+			qryAnzDs = frageSql(myDB, "SELECT Count(" + DatensammlungenFauna[i].DsBeziehungsfeldDs + ") AS Anzahl FROM " + DatensammlungenFauna[i].DsTabelle);
+			anzDs = qryAnzDs[0].Anzahl;
+			//alert(DatensammlungenFauna[i].DsTabelle + ": " + anzDs + " Datensätze. typeof anzDs = " + typeof(anzDs));
+			anzButtons = Math.ceil(anzDs/5000);
+			for (y = 1; y <= anzButtons; y++) {
+				html += "<button id='";
+				html += DatensammlungenFauna[i].DsTabelle + y;
+				html += "' name='SchaltflächeFaunaDatensammlung' Tabelle='" + DatensammlungenFauna[i].DsTabelle + "' Anz='" + y + "' Von='" + anzButtons + "'>";
+				html += DatensammlungenFauna[i].DsName + " (" + y + "/" + anzButtons + ")";
+				html += "</button>";
+			}
+		}
+		$("#SchaltflächenFaunaDatensammlungen").html(html);
 	} else {
 		alert("Bitte den Pfad zur .mdb erfassen");
 	}
